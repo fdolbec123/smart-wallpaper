3e5b7fcd2c84e15b7eb6feb2279e0ff1d763e439
Francis Dolbec <fdolbec123@gmail.com>
2021-03-07 03:02:03 -0500

    Add a initial_setup function in case it's needed in the future. For example, it could create a folder so every custom wallpapers would be located in that folder...

--- macOS/macOS_main.py
+++ macOS/macOS_main.py
@@ -1,2 +1,7 @@
 def main():
     print("We are on macOS!!!")
+
+
+def initial_setup():
+    pass
+    # In case we need to do an inital setup, for example, create a folder in the ~/Pictures/ folder...
