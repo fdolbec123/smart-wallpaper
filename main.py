dd98ad68e804e0bf99e94acf76b94571eeddcf91
Francis Dolbec <fdolbec123@gmail.com>
2021-03-07 11:57:52 -0500

    Preparing for cross-platform development.

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ hostOS = platform.system()
 print(hostOS)
 if hostOS == "Darwin":
     macOS_main.initial_setup()
+elif hostOS == "Linux":
+    pass
+elif hostOS == "Windows":
+    pass
+else:
+    print("Sorry, but this OS is not supported. This program is going to quit.")
