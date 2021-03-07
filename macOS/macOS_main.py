6e9f1d63ef832399c66da5857e17470bf1a7c80b
Francis Dolbec <fdolbec123@gmail.com>
2021-03-07 03:17:21 -0500

    - Imported PyQT5 - Created a blank window.

--- macOS/macOS_main.py
+++ macOS/macOS_main.py
@@ -1,5 +1,11 @@
+from PyQt5.QtWidgets import *
 def main():
     print("We are on macOS!!!")
+    main_app = QApplication()
+    main_window = QWidget()
+    main_window.show()
+
+    main_app.exec_()
 
 
 def initial_setup():
