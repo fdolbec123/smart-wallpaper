cf0c7f30e683243010df7a0d6867a51337732224
Francis Dolbec <fdolbec123@gmail.com>
2021-03-07 04:01:07 -0500

    Check in initial_setup if the app directory is already there, then send to the main window.

--- macOS/macOS_main.py
+++ macOS/macOS_main.py
@@ -1,13 +1,28 @@
 from PyQt5.QtWidgets import *
+import os
+# End of imports
+
+
+# main function
 def main():
     print("We are on macOS!!!")
     main_app = QApplication([])
     main_window = QWidget()
+    main_window.setWindowTitle("Smart Wallpaper")
     main_window.show()
 
     main_app.exec_()
 
 
+# initial_setup function
+# Used to make sure the app works properly.
 def initial_setup():
-    pass
+    custom_wallpaper_folder_path = "~/Pictures/Smart-Wallpaper/"
+    my_custom_dir = os.path.expanduser(custom_wallpaper_folder_path)
+    if os.path.isdir(my_custom_dir):
+        print("The folder is already there! Yay!")
+        print(os.path.expanduser(custom_wallpaper_folder_path))
+    else:
+        print("Need to create the folder so the app can work properly.")
+    main()
     # In case we need to do an inital setup, for example, create a folder in the ~/Pictures/ folder...
